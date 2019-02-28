b044d5c7608ecc8f59fa35783cb91287f02a56cb
rosaann <147429240@qq.com>
2019-02-28 11:48:37 +0800

    d

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python tools/find_duplicate_images.py
 python tools/stratified_split.py
-python stratified_split.py --use_external=0
+python tools/stratified_split.py --use_external=0
 python train.py --config=configs/inceptionv3.attention.policy.per_image_norm.1024.yml
